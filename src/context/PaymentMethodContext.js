1ffcc7cc696c0c1f78c31d22f889bad9e6a14617
hectorruizlaph <hectorruizlaph@gmail.com>
2022-08-01 10:37:27 -0600

    typescript installed

--- src/context/PaymentMethodContext.js
+++ src/context/PaymentMethodContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { data } from "../data";
+import { data } from "../data.tsx";
 
 export const PaymentMethodsContext = createContext();
 
---
+++ src/data.tsx
@@ -0,0 +1,50 @@
+import mastercard from "./images/mastercard.png";
+import paypal from "./images/paypal.png";
+import stripe from "./images/stripe.png";
+
+interface Data {
+  method: { text: string, image: string },
+  number: string | number,
+  amount: string | number,
+  expiration: string | number,
+  active: boolean,
+  id: number,
+  address?: string | number,
+  payment?: string | number,
+}
+
+export const data: Data[] = [
+  {
+    method: { text: "MASTERCARD", image: mastercard },
+    number: "****2890",
+    amount: "15,000",
+    expiration: "07 /2030",
+    active: true,
+    id: 1,
+    address: 5
+  },
+  {
+    method: { text: "PAYPAL", image: paypal },
+    number: "****6546",
+    amount: "15,000",
+    expiration: "04 / 225",
+    active: false,
+    id: 2,
+  },
+  {
+    method: { text: "STRIPE", image: stripe },
+    number: "****6546",
+    amount: "15,000",
+    expiration: "04 / 225",
+    active: false,
+    id: 3,
+  },
+  {
+    method: { text: "PAYPAL", image: paypal },
+    number: "****7644",
+    amount: "15,000",
+    expiration: "04 / 225",
+    active: false,
+    id: 4,
+  },
+];
